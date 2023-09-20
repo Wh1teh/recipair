e0daa4fc3513725d7c94531e24d03cc1a21af863
Miika Tiihonen <49746961+Wh1teh@users.noreply.github.com>
2023-09-20 22:17:41 +0300

    working on splash page

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,12 @@ var router = express.Router();
 const buildHTML = require('../joinHTML.js');
 const path = __dirname + '/../public/html/pages/index/';
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
 router.get('/', (req, res) => {
-  console.log("AA");
   const articles = [path + 'index.html'];
-  res.end(buildHTML.combineHTML(articles));
+  res.end(buildHTML.combineHTMLcustom(
+    [buildHTML.getHead(), buildHTML.getNav(), buildHTML.getHeader(),
+    buildHTML.getSections(articles), buildHTML.getFooter()]
+  ));
 });
 
 module.exports = router;
