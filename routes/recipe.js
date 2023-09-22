3831279c10341da04765419c73904bf6c8f4ec9b
Miika Tiihonen <49746961+Wh1teh@users.noreply.github.com>
2023-09-22 14:54:34 +0300

    rudimentary get request works

--- routes/recipe.js
+++ routes/recipe.js
@@ -2,27 +2,16 @@ const express = require('express');
 const router = express.Router();
 const recipe = require('../models/recipe_model');
 
-router.get('/', function (request, response) {
-    recipe.getAll(function (err, dbResult) {
+router.get('/', function (req, res) {  
+    recipe.getById(req.query.id, function (err, dbResult)  {
         if (err) {
-            response.json(err);
+            res.json(err);
         }
         else {
-            response.json(dbResult);
+            res.json(dbResult);
         }
     })
 });
-
-router.get('/:id',
-    function (request, response) {
-        recipe.getById(request.params.id, function (err, dbResult) {
-            if (err) {
-                response.json(err);
-            } else {
-                response.json(dbResult[0]);
-            }
-        })
-    });
 
 
 module.exports = router;
