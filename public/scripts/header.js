8e913b6a67b8fd6bc3c1fb22aedd63fd725401c5
Miika Tiihonen <49746961+Wh1teh@users.noreply.github.com>
2023-09-26 00:58:09 +0300

    more css and aspect ratio stuff

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -44,14 +44,14 @@ splashInput.addEventListener("focus", function () {
         splashTitles[index].style.marginTop = "0";
     }
 
-    splashContent.style.transform = 
-    'translateY(' + (splashTitles.length - 1) * (TITLE_OFFSET * TITLE_OFFSET_MULTIPLIER) + 'dvw)';
+    splashContent.style.transform =
+        'translateY(' + (splashTitles.length - 1) * (TITLE_OFFSET * TITLE_OFFSET_MULTIPLIER) + 'dvw)';
 });
 
 splashInput.addEventListener("blur", function () {
     for (let index = 0; index < splashTitles.length; index++) {
-        splashTitles[index].style.marginTop = 
-        ((splashTitles.length - index - 1) * (TITLE_OFFSET * TITLE_OFFSET_MULTIPLIER)) + 'dvw';
+        splashTitles[index].style.marginTop =
+            ((splashTitles.length - index - 1) * (TITLE_OFFSET * TITLE_OFFSET_MULTIPLIER)) + 'dvw';
     }
 
     splashContent.style.transform = 'translateY(' + 0 + 'dvh)';
@@ -246,17 +246,29 @@ function isJson(jsonString) {
 
 //mobile stuff below
 
-// Function to change h1 margin-top
-function changeMarginTop(media) {
-    const h1Element = document.querySelector("h1");
+// ignore keyboard
+if('virtualKeyboard' in navigator) {
+    navigator.virtualKeyboard.overlaysContent = true;
+  }
 
-    // Check if aspect ratio is below 6/5
-    if (media.matches) {
-        console.log("detected <6/5 aspect ratio");
-        TITLE_OFFSET_MULTIPLIER = 2.5;
-    } else {
-        console.log("detected >6/5 aspect ratio");
-        TITLE_OFFSET_MULTIPLIER = 1.0
+// Function to change h1 margin-top
+function changeTitleMarginTop(aspectQueryList) {
+    //find first aspect ratio that matches
+    switch (aspectQueryList.findIndex((item) => item.matches === true)) {
+        case 0:
+            console.log("detected <2/3 aspect ratio");
+            TITLE_OFFSET_MULTIPLIER = 2.5;
+            break;
+
+        case 1:
+            console.log("detected <6/5 aspect ratio");
+            TITLE_OFFSET_MULTIPLIER = 1.5;
+            break;
+
+        default:
+            console.log("default? aspect ratio");
+            TITLE_OFFSET_MULTIPLIER = 1.0
+            break;
     }
 
     const titleElements = document.querySelectorAll('.splash-content h1');
@@ -264,17 +276,25 @@ function changeMarginTop(media) {
     for (let index = 0; index < DUPE_AMOUNT; index++) {
         if (index + 1 != DUPE_AMOUNT) {
             //don't apply to last title (this is gonna be the main one)
-            titleElements[index].style.marginTop = 
-            ((DUPE_AMOUNT - index - 1) * (TITLE_OFFSET * TITLE_OFFSET_MULTIPLIER)) + 'dvw';
+            titleElements[index].style.marginTop =
+                ((DUPE_AMOUNT - index - 1) * (TITLE_OFFSET * TITLE_OFFSET_MULTIPLIER)) + 'dvw';
         }
     }
 }
 
 // Detect changes in aspect ratio
-const mediaQuery = window.matchMedia("(max-aspect-ratio: 6/5)");
+const aspectRatios = ['2/3', '6/5', '1000/1'];
+var aspectQueryList = [];
+
+for (let i = 0; i < aspectRatios.length; i++) {
+    const aspectQuery = window.matchMedia(`(max-aspect-ratio: ${aspectRatios[i]})`);
+    aspectQueryList.push(aspectQuery);
+
+    // Add event listener for changes in aspect ratio
+    aspectQuery.addEventListener("change", function () {
+        changeTitleMarginTop(aspectQueryList);
+    });
+}
 
 // Initial check
-changeMarginTop(mediaQuery);
-
-// Add event listener for changes in aspect ratio
-mediaQuery.addEventListener("change", changeMarginTop);
+changeTitleMarginTop(aspectQueryList);
