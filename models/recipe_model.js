18644b9ce01f74e4608f8a1689c07f2aebba51c8
Miika Tiihonen <49746961+Wh1teh@users.noreply.github.com>
2023-09-27 11:09:46 +0300

    regex to find by ingredient

--- models/recipe_model.js
+++ models/recipe_model.js
@@ -5,17 +5,20 @@ const recipe = {
     return db.query('select * from recipe', callback);
   },
   getById: function (ids, callback) {
-    // Convert the array of IDs into a comma-separated string
-    if(Array.isArray(ids)) {
-      ids.join(',');
+    // Convert the array of IDs into a pipe-separated string
+    var params;
+    if (Array.isArray(ids)) {
+      params = ids.join('|');
+    } else {
+      params = ids;
     }
-    console.log(ids);
 
     //select all recipes that contain the keywords
     const query = 'SELECT DISTINCT r.* FROM recipe r ' +
-    'INNER JOIN recipe_ingredient ri ON r.id = ri.recipe_id ' +
-    'WHERE ri.name IN (?)';
-    const params = [ids];
+      'INNER JOIN recipe_ingredient ri ON r.id = ri.recipe_id ' +
+      'WHERE ri.name REGEXP "(?)"';
+
+    console.log(query + ", " + params);
 
     // Execute the query
     return db.query(query, params, callback);
