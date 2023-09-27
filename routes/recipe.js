5facdbfec7ce7b78b0436cf6b50740e38615f20e
Miika Tiihonen <49746961+Wh1teh@users.noreply.github.com>
2023-09-27 19:07:35 +0300

    slider works except when going to edge

--- routes/recipe.js
+++ routes/recipe.js
@@ -5,7 +5,8 @@ const recipe = require('../models/recipe_model');
 router.get('/', function (req, res) {
     recipe.getById(req.query.id, function (err, dbResult) {
         if (err) {
-            res.json(err);
+            // res.json(err);
+            res.status(500).json({ error: 'Internal Server Error' });
         }
         else {
             res.json(dbResult);
