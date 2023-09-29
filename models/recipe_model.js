9f49442c013a3434d34f8d353a44a86ae7a0b25d
Miika Tiihonen <49746961+Wh1teh@users.noreply.github.com>
2023-09-29 16:33:27 +0300

    make search actually work as intended

--- models/recipe_model.js
+++ models/recipe_model.js
@@ -5,23 +5,27 @@ const recipe = {
     return db.query('select * from recipe', callback);
   },
   getById: function (ids, callback) {
-    // Convert the array of IDs into a pipe-separated string
-    var params;
-    if (Array.isArray(ids)) {
-      params = ids.join('|');
-    } else {
-      params = ids;
+    //select all recipes that contain the keywords
+    var query =
+      'SELECT DISTINCT r.* \n' +
+      'FROM recipe r \n';
+
+    var midQuery = 'INNER JOIN recipe_ingredient ri0 ON r.id = ri0.recipe_id \n';
+    var tailQuery = 'WHERE ri0.name REGEXP ? \n';
+
+    if(Array.isArray(ids)) {
+      for (let index = 1; index < ids.length; index++) {
+        midQuery += 'INNER JOIN recipe_ingredient ri' + index + ' ON r.id = ri' + index + '.recipe_id \n';
+        tailQuery += 'AND ri' + index +'.name REGEXP ? \n';
+      }
     }
 
-    //select all recipes that contain the keywords
-    const query = 'SELECT DISTINCT r.* FROM recipe r ' +
-      'INNER JOIN recipe_ingredient ri ON r.id = ri.recipe_id ' +
-      'WHERE ri.name REGEXP ?';
+    query += midQuery + tailQuery;
 
-    console.log(query + ", " + params);
+    console.log(query + ", " + ids);
 
     // Execute the query
-    return db.query(query, params, callback);
+    return db.query(query, ids, callback);
   },
 };
 module.exports = recipe;
