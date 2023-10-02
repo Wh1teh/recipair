644ee6eda3c450d58655030b32cc2625e9fcdc69
Miika Tiihonen <49746961+Wh1teh@users.noreply.github.com>
2023-10-03 01:54:13 +0300

    add urls featured hrefs

--- routes/recipe.js
+++ routes/recipe.js
@@ -6,7 +6,16 @@ router.get('/', function (req, res) {
     if (req.query.id) {
         recipe.getById(req.query.id, function (err, dbResult) {
             if (err) {
-                // res.json(err);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+            else {
+                res.json(dbResult);
+            }
+        })
+    }
+    else if (req.query.param) {
+        recipe.getByParameters(req.query.param, function (err, dbResult) {
+            if (err) {
                 res.status(500).json({ error: 'Internal Server Error' });
             }
             else {
@@ -23,6 +32,33 @@ router.get('/', function (req, res) {
             }
         })
     }
+});
+
+const buildHTML = require('../joinHTML.js');
+const path = __dirname + '/../public/html/pages/recipe/';
+
+router.get('/:id', (req, res) => {
+    console.log(req)
+    recipe.getById(req.params.id, function (err, dbResult) {
+        if (err) {
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            const recipe = dbResult[0];
+            console.log("AAAAAAA", recipe)
+            const htmlSection = `
+  <section>
+    <h1>${recipe.title}</h1>
+    <p>${recipe.content}</p>
+    <p>Written by: ${recipe.writer_id}</p>
+  </section>
+`;
+
+            res.end(buildHTML.combineHTMLcustom(
+                [buildHTML.getHead(), buildHTML.getNav(), /*buildHTML.getHeader(),*/
+                htmlSection, buildHTML.getFooter()]
+            ));
+        }
+    })
 });
 
 
