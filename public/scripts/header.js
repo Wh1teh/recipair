ab6c15052a04af0dc740c72f0a6ffaa9d1d6abc7
Miika Tiihonen <49746961+Wh1teh@users.noreply.github.com>
2023-10-14 21:10:30 +0300

    new desktop footer animation

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -10,8 +10,7 @@ window.addEventListener('scroll', function () {
         + ') translateY(' + (scrolled * PARALLAX_MULTIPLIER) + 'px)';
 });
 
-
-
+//footer desktop
 var isAtFooter = false;
 window.addEventListener('wheel', function (e) {
     //check based on aspect ratio
@@ -24,28 +23,69 @@ window.addEventListener('wheel', function (e) {
     var deltaY = e.deltaY;
     var scrollingDown = deltaY > 0 ? true : false;
 
-    //get side elements
-    var footer = document.querySelector("footer");
-    var featured = document.querySelector(".splash-featured");
+    //get elements
+    var footer = document.querySelector(".footer-container");
+    var featured = document.querySelector(".splash-featured-container");
 
-    //get footer height
-    var footerHeight = footer.offsetHeight;
 
-    var moveFooterTo = scrollingDown ? footerHeight * -1 : 0;
+    const ROTATION_TRANSITION = 1000;
 
-    //move side elements
-    footer.style.transform =
-        'translate(0, calc(' + moveFooterTo + 'px  - '
-        + (scrollingDown ? 5 : 0) + 'dvh))';
-
-    featured.style.transition = "500ms";
+    featured.style.transition = ROTATION_TRANSITION + "ms";
+    footer.style.transition = ROTATION_TRANSITION + "ms";
     if (scrollingDown) {
-        featured.style.top = 'var(--nav-height)';
+        //move elements
+        footer.style.transform = 'rotate3d(1, 0, 0, 360deg)';
+        featured.style.transform = 'rotate3d(1, 0, 0, 180deg)';
+        setTimeout(() => {
+            // document.querySelector(".splash-featured").style.opacity = '0';
+        }, ROTATION_TRANSITION);
+        
     } else {
-        featured.style.top = '';
+        //move elements
+        footer.style.transform = 'rotate3d(1, 0, 0, 180deg)';
+        featured.style.transform = 'rotate3d(1, 0, 0, 0deg)';
+        setTimeout(() => {
+
+        }, ROTATION_TRANSITION);
     }
 
 }, false);
+
+//old footer behavior on desktop
+// var isAtFooter = false;
+// window.addEventListener('wheel', function (e) {
+//     //check based on aspect ratio
+//     var isMobile = window.matchMedia("(max-aspect-ratio: 6/5)").matches;
+//     if (isMobile) {
+//         return;
+//     }
+
+//     //determine scroll direction
+//     var deltaY = e.deltaY;
+//     var scrollingDown = deltaY > 0 ? true : false;
+
+//     //get side elements
+//     var footer = document.querySelector("footer");
+//     var featured = document.querySelector(".splash-featured");
+
+//     //get footer height
+//     var footerHeight = footer.offsetHeight;
+
+//     var moveFooterTo = scrollingDown ? footerHeight * -1 : 0;
+
+//     //move side elements
+//     footer.style.transform =
+//         'translate(0, calc(' + moveFooterTo + 'px  - '
+//         + (scrollingDown ? 5 : 0) + 'dvh))';
+
+//     featured.style.transition = "500ms";
+//     if (scrollingDown) {
+//         featured.style.top = 'var(--nav-height)';
+//     } else {
+//         featured.style.top = '';
+//     }
+
+// }, false);
 
 
 //splash title generation
